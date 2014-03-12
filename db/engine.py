e787e12eb38e5e168f70d53e840d1d563a7505c8
Alex Freas <akfreas@gmail.com>
2014-03-12 18:16:31 +0100

    Moved mapping.

--- db/engine.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy import create_engine
-import settings
-engine = create_engine('postgresql:///%s:%s@%s:%s/karten' % (settings.PSQL_USERNAME, settings.PSQL_PASSWORD, settings.PSQL_SERVER, settings.PSQL_PORT))
-
--- db/settings.py
+++
@@ -1,4 +0,0 @@
-PSQL_USERNAME = 'akfreas'
-PSQL_PASSWORD = 'kasashi'
-PSQL_SERVER = 'localhost'
-PSQL_PORT = 5432
