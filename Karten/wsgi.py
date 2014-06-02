fef114f0131b0a6c887a7e6cfdd4bb2b909a3e5d
Alexander Freas <alex@komoot.de>
2014-06-03 01:07:43 +0200

    Added wsgi.py.

---
+++ Karten/wsgi.py
@@ -0,0 +1,7 @@
+import os
+import sys
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+sys.path.append(PROJECT_PATH)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Karten.settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
