39ef41256572ecea534fd5867b7087e045438f69
Alexander Freas <alex@komoot.de>
2014-10-12 12:31:09 +0200

    [testutils] Added.

---
+++ Karten/Karten/tests/testutils.py
@@ -0,0 +1,31 @@
+from Karten.models import *
+import random
+import string
+from django.db.models.signals import post_save
+
+def rnd():
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(random.randrange(4, 12)))
+
+def create_user(username, password):
+
+    new_user = KartenUser(username=username, date_joined=timezone.now())
+    new_user.save()
+    new_user.set_password(password)
+    new_user.save()
+    return new_user
+
+def create_dummy_users(count=10, username_prefix=None):
+    
+    users = []
+    for i in range(0, count):
+        username = rnd()
+        if username_prefix is not None:
+            username = "%s%s" % (username_prefix, username)
+        users.append(create_user(username, "password"))
+
+    return users
+
+def disconnect_couchdb_signals():
+    post_save.disconnect(create_auth_token, sender=KartenUser)
+    post_save.disconnect(update_couchdb_password, sender=Token)
+
